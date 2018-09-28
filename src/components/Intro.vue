47f87c38428071fa9750e9cc2b2efc63e651fb50
Luiz <luiz.gustavo358@gmail.com>
2018-09-28 17:18:22 -0300

    adicionando 2 componentes

---
+++ src/components/Intro.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium finibus viverra. Morbi sit amet venenatis nulla. Vestibulum maximus sit amet magna et varius. Nullam at elementum justo, id lacinia libero. Ut bibendum dui id blandit hendrerit. Nulla commodo nisi non pulvinar vestibulum. Integer gravida vitae massa et commodo. Sed fermentum nisi diam, vel varius lacus ultricies id. Proin lectus dolor, luctus ac pellentesque quis, aliquet in odio. Curabitur aliquet mollis egestas. Sed ut nisl in ante lobortis accumsan. Nam scelerisque purus vel diam condimentum, vel ultricies mauris vehicula. Aenean quis pellentesque libero, in ultrices metus.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Intro'
+}
+</script>
+
+<style scoped>
+    p {
+        color: lightslategrey;
+    }
+</style>
